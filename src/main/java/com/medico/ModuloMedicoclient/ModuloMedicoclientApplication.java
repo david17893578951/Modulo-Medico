54951dce73e25320f2d0b850a7e805a7048c670e
jdfloresl <jdfloresl@utn.edu.ec>
2019-09-11 18:14:15 -0500

    ultimo

--- src/main/java/com/medico/ModuloMedicoclient/ModuloMedicoclientApplication.java
+++ src/main/java/com/medico/ModuloMedicoclient/ModuloMedicoclientApplication.java
@@ -2,20 +2,19 @@ package com.medico.ModuloMedicoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 @SpringBootApplication
-@RestController
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+
 public class ModuloMedicoclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuloMedicoclientApplication.class, args);
 	}
-	
-	@RequestMapping("/api")
-	  public String findAll(){
-	    return "Hello World!";
-	  }
-
 }
